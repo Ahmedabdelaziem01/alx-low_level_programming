713a4c5fdd4152e0c923cfe2bc991d547cd83fb5
Ahmedabdelaziem01 <ahmedabdelaziem001@gmail.com>
2023-07-15 15:54:31 -0700

    aa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
-
 /**
- * min - Entry point
- * 
- * Description: A C program that prints with put function
- *
- * Return: Alawys 0 (success)
+ *helli c
 */
-
-int main (void)
+int main(void)
 {
-puts("i\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
