6c86602d9a19e2860530cefa83ac5c2d27bb803c
Ahmedabdelaziem01 <ahmedabdelaziem001@gmail.com>
2023-07-16 09:19:15 -0700

    aa

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
- * Description: A C progam that prints with printf function.
+ *  Description: A C program that prints with put function
  *
- * Return: Always 0 (success)
- */
-
+ *  Return: Alawys 0 (success)
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art.\n");
+putis("\"with proper grammar, but the outcome is a piece of art.\n");
 return (0);
 }
