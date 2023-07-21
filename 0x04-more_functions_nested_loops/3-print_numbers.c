6a23f91dcd7360bc1730d8f0dc61c335265761e6
Ahmedabdelaziem01 <ahmedabdelaziem001@gmail.com>
2023-07-21 14:18:59 -0700

    a

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 - 9
+ *		only useing _putchar twice
+ *
+ * Return: 1 if its uppercase. 0 if not
+*/
+
+void print_numbers(void)
+{
+	int num = 0;
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
+	_putchar('\n');
+}
