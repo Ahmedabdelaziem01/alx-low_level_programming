fcfac0451d97a3e0eb6d4f51d979af9a7471559f
Ahmedabdelaziem01 <ahmedabdelaziem001@gmail.com>
2023-07-21 13:17:34 -0700

    a

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,10 +7,10 @@
  *
  * Return: 1 if its uppercase. 0 if not
 */
-{
-int _isupper(int c)
 
-	if (c >= 65 && c <= 90)
+int _isupper(int c)
+{
+	if (c >= 65 && c <= *0)
 		return (1);
 	else
 		return (0);
